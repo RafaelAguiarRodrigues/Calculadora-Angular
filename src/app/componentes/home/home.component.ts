605a0de21f0543588dd49a4a7ec1b2008bf1acfd
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2023-08-09 17:27:03 -0300

    logica pronta

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  title = "calculadora";
+  resultado = "";
+  valorAtual = '';
 
+  escreverInput(valor: string) {
+    this.valorAtual += valor;
+    this.resultado = this.valorAtual;
+  }
+
+  calculaResultado() {
+    this.resultado = eval(this.valorAtual);
+    this.valorAtual = this.resultado;
+  }
+
+  limpaInput() {
+    this.valorAtual = '';
+    this.resultado = this.valorAtual;
+  }
+
+  apagar() {
+    this.valorAtual = this.valorAtual.slice(0, -1);
+    this.resultado = this.valorAtual;
+  }
 }
